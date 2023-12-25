cd05e5af5ad3d911060759c47616a4eaa83ca777
Ruslan_BRV <ruslan10897@mail.ru>
2023-12-25 21:17:04 +0300

    Создал таблицу продукты и вывел данные в шаблон html

--- web/models.py
+++ web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(verbose_name="Наименование", max_length=100)
+    price = models.FloatField(verbose_name="Цена")
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return self.name
--- web/views.py
+++ web/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from web.models import Product
 
 # Create your views here.
 def index(request: HttpRequest):
-    return render(request, "index.html")
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "index.html", context=context)
 
 def contacts(request: HttpRequest):
     return render(request, "contacts.html")
